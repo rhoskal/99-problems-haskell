7ce0c9d571eb832e06d907f902302025d62b422f
Hans Hoffman <github.backrest158@passmail.net>
2024-06-26 11:18:18 -0300

    no units

--- test/MyLibSpec.hs
+++ test/MyLibSpec.hs
@@ -13,17 +13,17 @@ import Test.Hspec
 spec :: Spec
 spec = do
   it "[01] Should return the last element of a list" $ do
-    myLast [] `shouldBe` (Nothing :: Maybe ())
+    myLast [] `shouldBe` (Nothing :: Maybe String)
     myLast ["a"] `shouldBe` (Just "a" :: Maybe String)
     myLast [1, 2, 3] `shouldBe` (Just 3 :: Maybe Int)
 
   it "[02] Should return the last two elements of a list" $ do
-    lastTwo [] `shouldBe` (Nothing :: Maybe [()])
+    lastTwo [] `shouldBe` (Nothing :: Maybe [Bool])
     lastTwo [True] `shouldBe` (Nothing :: Maybe [Bool])
     lastTwo [1, 2, 3] `shouldBe` (Just [2, 3] :: Maybe [Int])
 
   it "[03] Should return the nth element of a list" $ do
-    elementAt 2 [] `shouldBe` (Nothing :: Maybe ())
+    elementAt 2 [] `shouldBe` (Nothing :: Maybe Int)
     elementAt (-2) [1, 2] `shouldBe` (Nothing :: Maybe Int)
     elementAt 2 [1, 2] `shouldBe` (Just 2 :: Maybe Int)
     elementAt 2 ['a' .. 'e'] `shouldBe` (Just 'b' :: Maybe Char)
@@ -34,7 +34,7 @@ spec = do
     myLength [(1 :: Int) .. 10] `shouldBe` 10
 
   it "[05] Should return the elements in a list reversed" $ do
-    myReverse [] `shouldBe` ([] :: [()])
+    myReverse [] `shouldBe` ([] :: [Int])
     myReverse [1 .. 5] `shouldBe` ([5, 4, 3, 2, 1] :: [Int])
     myReverse "A man, a plan, a canal, panama!" `shouldBe` ("!amanap ,lanac a ,nalp a ,nam A" :: [Char])
 
